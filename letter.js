8eb75dffea4e4557205d7d0b98752983fde03f86
Jens <jens@techgrounds.nl>
2022-04-17 20:54:47 +0200

    word up?

--- letter.js
+++ letter.js
@@ -1,9 +1,11 @@
 import { getRandChar } from "./main.js"
 
 export default class Letter {
-    constructor(position, options) {
+    constructor(ctx, y, options) {
+        this.context = ctx
         this.char = getRandChar();
-        this.y = position;
+        this.x = options.position * options.fontSize
+        this.y = y * options.fontSize;
         this.opacity = Math.floor(Math.random() * 100) / 100
         this.charChangeRate = options.charChangeRate
         this.opacityChangeRate = options.opacityChangeRate
@@ -11,13 +13,13 @@ export default class Letter {
         this.fadedDecimalOdds = options.fadedPercentage / 100 + options.hiddenPercentage / 100
     }
 
-    draw(ctx, x, y) {
+    draw() {
         const newCharChance = Math.floor(Math.random() * 100)
         if (newCharChance < this.charChangeRate) this.char = getRandChar();
         const newOpacityChance = Math.floor(Math.random() * 100)
         if (newOpacityChance < this.opacityChangeRate) this.opacity = Math.floor(Math.random() * 100) / 100
-        ctx.globalAlpha = this.opacity < this.hiddenDecimalOdds ? 0 : this.opacity < this.fadedDecimalOdds ? 0.5 : 1;
-        ctx.fillText(this.char, x, y)
-        ctx.globalAlpha = 1;
+        this.context.globalAlpha = this.opacity < this.hiddenDecimalOdds ? 0 : this.opacity < this.fadedDecimalOdds ? 0.5 : 1;
+        this.context.fillText(this.char, this.x, this.y)
+        this.context.globalAlpha = 1;
     }
 }
